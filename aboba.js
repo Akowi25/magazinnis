4d85223403f0fa4abd12e80b694c0dd1897d9373
Akowi25 <164137177+Akowi25@users.noreply.github.com>
2024-12-23 20:57:07 +0500

    Add files via upload

---
+++ aboba.js
@@ -0,0 +1,11 @@
+console.log("aboba.js")
+alert("aboba")
+prompt("ты точно абоба? ")
+alert("просто перейди по ссылке https://youtu.be/dQw4w9WgXcQ?si=IbkuQ0v9iJpnFm8d ")
+let name = "Akbarshokh"
+let age = 23;
+let iswriting = true;
+let userhome = null;
+
+
+console.log(name, age)
